b5fefeac0c43d00ac3ad1827f047a9c485e1646c
rmpi <ryanmakarem91@gmail.com>
2021-10-11 03:32:16 -0400

    add --all

--- Automation/Jobs/J_GitPush.py
+++ Automation/Jobs/J_GitPush.py
@@ -8,21 +8,17 @@ def gitPush():
         subprocess.run(["git add --all"],shell=True,check=True,cwd=_P.RMPI_MASTER_PATH)
         subprocess.run(["git status"],shell=True,check=True,cwd=_P.RMPI_MASTER_PATH)
         subprocess.run(["git commit -am 'add --all'"],shell=True,check=True,cwd=_P.RMPI_MASTER_PATH)
-        #subprocess.run(["git push -u origin master"],shell=True,check=True,cwd=_P.RMPI_MASTER_PATH)
-#         subprocess.run(["git push -f origin master"],shell=True,check=True,cwd=_P.RMPI_MASTER_PATH)
         try:
             child = pexpect.spawn("git push -u origin master")
             child.logfile_read = sys.stdout
             i= child.expect(["Are you sure you want to continue connecting","Enter passphrase"],timeout=5)
-            print(i)
             if i==0:
                 time.sleep(0.05)
-#                 child.sendline("yes")
-            #v= child.expect(["Are you sure you want to continue connecting","Enter passphrase"],timeout=5)
+                child.sendline("yes")
         except:
             try:
                 pass
-                #subprocess.run(["git push -f origin master"],shell=True,check=True,cwd=_P.RMPI_MASTER_PATH)
+                subprocess.run(["git push -f origin master"],shell=True,check=True,cwd=_P.RMPI_MASTER_PATH)
             except Exception as e:
                 print(e)
     except Exception as e:
